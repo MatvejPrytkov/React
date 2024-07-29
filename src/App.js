3f30587dc61f31112581873df4e04c822efd2060
MatvejPrytkov <matvej.ptykov.02@bk.ru>
2024-07-29 15:40:47 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+const exp1 = <div className="some">2 + 3 = {2 + 3}</div>
+const date = new Date();
+const odd = <div>нечётный</div>
+const even = <div>чётный</div>
+const result = date.getHours() % 2 ? odd : even
+const imageURL = 'https://placekitten.com/100/100';
+const image = <img src={imageURL} />;
+// const jsxItems = [
+//   <h1>Заголовок1</h1>,
+//   <h2>Заголовок2</h2>,
+//   <h3>Заголовок3</h3>,
+//   image,
+//   result
+// ]
+
+const users = [
+  {
+    id:1235,
+    name:'Efim',
+    job:'Efim'
+  },
+  {
+    id:1236,
+    name:'Matvej',
+    job:'Python-programmer'
+  },
+  {
+    id:1238,
+    name:'Alina',
+    job:'House'
+  },
+]
+const jsxItems = users.map( user =>{
+return <div className='user' key={user.id}>
+
+  <h3>{user.name}</h3>
+  <p>{user.job}</p>
+</div>
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +55,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        {jsxItems}
+        {image}
+        <h1>Привет, мир!</h1>
+        {exp1}
+        {date.toLocaleTimeString()}
+ {result}
+
       </header>
     </div>
   );
